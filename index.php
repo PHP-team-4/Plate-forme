ce71e074ccfb65c8159f3698fddee6932ccce129
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2021-11-28 14:38:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Kaisei+HarunoUmi&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="css/main2.css" />
-    <title>PlateForme GROPE TADLAOUI</title>
+    <title>PlateForme</title>
   </head>
   <body>
    
