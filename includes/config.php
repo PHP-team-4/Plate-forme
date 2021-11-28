c1f3cb3a2a5c1b6082a5e5cb7e3ab863eab5389a
imade-janaoui <imadejanaoui@gmail.com>
2021-11-28 14:22:35 +0100

    config base de donnee

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$sname= "localhost";
+$usrename= "root";
+$password = "";
+$db_name = "plateforme";
+
+$conn = mysqli_connect($sname, $username, $password, $db_name);
--- includes/uploades/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-$db_name = "plateforme_tadlaoui";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
